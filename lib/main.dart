ebdd0afb3a6a7e184650f6cd68a3b87a543c2203
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-09 00:08:57 -0700

    Separate Translator and Dictionary part and connect them with main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,43 @@
+import 'package:dictionarytranslatorapp/screens/dictionary.dart';
+import 'file:///G:/esunb/Github/dictionary_translator_app/lib/screens/translator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyTranslator());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyTranslator extends StatefulWidget{
+  @override
+  _MyTranslatorState createState() => _MyTranslatorState();
+}
+
+class _MyTranslatorState extends State<MyTranslator> {
+  int selectedIndex = 1;
+  List<Widget> pageList = [Translator(), Dictionary()];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        body: pageList[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              title: Text("Translator"),
+              icon: Icon(Icons.text_fields),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Translator"),
+              icon: Icon(Icons.library_books),
+            ),
+          ],
+          onTap: (index){
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+        ),
+      ),
+    );
   }
 }
