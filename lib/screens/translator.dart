c91bb0cdbceece0fb9cf56915ad76be3423e5bdf
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-09 03:39:22 -0700

    Build Layout and connect FormKey and TextEditingController

--- lib/screens/translator.dart
+++ lib/screens/translator.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class Translator extends StatefulWidget {
   @override
@@ -8,6 +10,144 @@ class Translator extends StatefulWidget {
 class _TranslatorState extends State<Translator> {
   @override
   Widget build(BuildContext context) {
-    return Text("translator");
+    Size size = MediaQuery.of(context).size;
+    String currentLang;
+    String targetLang;
+    GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    TextEditingController _starLangController = TextEditingController();
+    TextEditingController _targetController = TextEditingController();
+    double visualAreaHeight = size.height * 0.2;
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Container(
+            color: Color(0XFFc8dcfd),
+            child: Container(
+              width: size.width,
+              height: visualAreaHeight,
+              decoration: BoxDecoration(
+                color: Color(0xff001c2f),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Image.asset(
+                "assets/images/recording.gif",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Container(
+                  color: Color(0xff001c2f),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Color(0XFFc8dcfd),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 15,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.text_fields),
+                        DropdownButton(value: "English", items: [
+                          DropdownMenuItem<String>(
+                            value: "English",
+                            child: Text("English"),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: "Englis2h",
+                            child: Text("Englis2h"),
+                          ),
+                        ]),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: size.width,
+                    color: Color(0XFFc8dcfd),
+                    child: TextFormField(
+                      controller: _starLangController,
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                      ),
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                        hintText: "Start Language",
+                        border: InputBorder.none,
+                      ),
+                      onEditingComplete: () {},
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color(0XFFc8dcfd),
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(50),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Container(
+                  color: Color(0XFFc8dcfd),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: TextFormField(
+                    controller: _targetController,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 20,
+                      decoration: TextDecoration.none,
+                    ),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
+                      ),
+                      hintText: "Target Language",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
