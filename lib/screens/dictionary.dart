e1f47ebe391c905741c656edd3e3428ca06625cd
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-09 00:09:47 -0700

    Create Dictionary page

---
+++ lib/screens/dictionary.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Dictionary extends StatefulWidget {
+  @override
+  _DictionaryState createState() => _DictionaryState();
+}
+
+class _DictionaryState extends State<Dictionary> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("dictionary");
+  }
+}
