5730e951b868bb288645f68bcaadb5b89b9facd2
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-09 00:10:12 -0700

    Create Translator page

---
+++ lib/screens/translator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Translator extends StatefulWidget {
+  @override
+  _TranslatorState createState() => _TranslatorState();
+}
+
+class _TranslatorState extends State<Translator> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("translator");
+  }
+}
