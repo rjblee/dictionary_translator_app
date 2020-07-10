3a8686371ad944d7763f5faaa7c78b01d8fdd46a
Ralph Lee <ralph.lee@live.ca>
2020-07-09 20:53:46 -0700

    Implement an appbar with a TextFormField and search

--- lib/screens/dictionary.dart
+++ lib/screens/dictionary.dart
@@ -6,8 +6,57 @@ class Dictionary extends StatefulWidget {
 }
 
 class _DictionaryState extends State<Dictionary> {
+  String _url = "https://owlbot.info/api/v4/dictionary/";
+  String _token = "798c2e0afa8901dc9a9b02c64fab8a39c5e9394e";
+
+  TextEditingController _controller = TextEditingController();
+
+  _search() {}
+
   @override
   Widget build(BuildContext context) {
-    return Text("dictionary");
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Dictionary"),
+          backgroundColor: Colors.blueAccent,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(48),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 12, bottom: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: TextFormField(
+                      onChanged: (String text) {},
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: "Search for a Word",
+                        contentPadding: EdgeInsets.only(left: 24),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    _search();
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+        body: StreamBuilder(builder: null),
+      ),
+    );
   }
 }
